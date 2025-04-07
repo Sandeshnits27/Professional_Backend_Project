bfa99200111bbc57900947da629a7444ec3b5a9a
sandipan bisaws <biswassandipan058@gmail.com>
2025-04-08 00:37:05 +0530

    commit7

--- src/utils/asyncHandeler.js
+++ src/utils/asyncHandeler.js
@@ -1,5 +1,10 @@
 const asyncHandeler =() =>{}
  
+    const asyncHandler = (requestHandler) =>{
+        (req,res,next)=>{
+            Promise.resolve(requestHandler(requestHandler,res,next)).thencatch((err)=>next(err))
+        }
+    }
 
 
 export {asyncHandeler}
