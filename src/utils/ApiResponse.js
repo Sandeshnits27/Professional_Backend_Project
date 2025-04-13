1cf8ed7a143d074afeaed8ee251b4a21bf7329bb
Sandeshnits27 <biswassandipan058@gmail.com>
2025-04-13 21:54:03 +0530

    Delete src directory

--- src/utils/ApiResponse.js
+++
@@ -1,8 +0,0 @@
-class ApiRespose{
-    constructor (statusCode, data , massage="Success"){
-        this.statusCode = statusCode;
-        this.data=data;
-        this.massage=massage;
-        this.success = statusCode<400;
-    }
-}
